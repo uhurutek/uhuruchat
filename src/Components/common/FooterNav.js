73cdb23c2e64af39267139f9d7f42ad2771ff8c0
shant00 <sshanto.cse@gmail.com>
2023-12-27 21:52:33 +0600

    modify footer and nav

--- src/Components/common/FooterNav.js
+++ src/Components/common/FooterNav.js
@@ -1,80 +1,143 @@
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import { Box, Container, Grid, IconButton, Link, Typography } from '@mui/material';
-
-// Replace these with your own social media URLs
-const socialMediaLinks = {
-    facebook: '#',
-    twitter: '#',
-    instagram: '#',
-};
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Box, Container, Grid, Link, Typography } from '@mui/material';
 
 const FooterNav = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <Box
-            sx={{
-                bgcolor: '#9f4923',
-                color: 'white',
-                py: 3,
-                borderTop: '1px solid',
-                borderColor: 'divider',
-            }}
-        >
-            <Container maxWidth={false}>
-                <Grid container spacing={2} justifyContent="space-between">
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Typography variant="h6" color='white' gutterBottom>
-                            Brand Name
-                        </Typography>
-                        {/* Add your logo component or image here */}
+        <footer id="footer">
+            <Box sx={{ bgcolor: 'background.default' }}>
+                <Container maxWidth="lg">
+                    <Grid container spacing={3}>
+                        <Grid item lg={4} md={6}>
+                            <Box sx={{ py: 3 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    UhuruTek
+                                </Typography>
+                                <Typography variant="body1">
+                                    Dream big and allow UhuruTek to make your vision a ground-breaking reality. Experience a revolution that completely rewrites the rules of technology with our advanced solutions.
+                                </Typography>
+                            </Box>
+                        </Grid>
+
+                        <Grid item lg={2} md={6}>
+                            <Box sx={{ py: 3 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Useful Links
+                                </Typography>
+                                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                                    <li>
+                                        <Link href="#" color="inherit">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#about" color="inherit">
+                                            About us
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#technology" color="inherit">
+                                            Technology
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#services" color="inherit">
+                                            Services
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </Box>
+                        </Grid>
+
+                        <Grid item lg={3} md={6}>
+                            <Box sx={{ py: 3 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Our Services
+                                </Typography>
+                                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                                    <li>
+                                        <Link href="#services" color="inherit">
+                                            Software Development
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#services" color="inherit">
+                                            Web Development
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#services" color="inherit">
+                                            Mobile App Development
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#services" color="inherit">
+                                            AI/ML Solutions
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </Box>
+                        </Grid>
+
+                        <Grid item lg={3} md={6}>
+                            <Box sx={{ py: 3 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Contact Us
+                                </Typography>
+                                <Typography variant="body1">
+                                    DOM-INNO PATIO, 2 Siddeshwari Road, B-1, Dhaka, 1217, BD
+                                    <br />
+                                    <strong>Phone:</strong>
+                                    <Link href="tel:+8801846412513" color="inherit">
+                                        +880 1846-412513
+                                    </Link>
+                                    <br />
+                                    <strong>Email:</strong>
+                                    <Link href="mailto:info@uhurutek.com" color="inherit">
+                                        info@uhurutek.com
+                                    </Link>
+                                </Typography>
+                                <Box sx={{ mt: 3 }}>
+                                    <Typography variant="h6" gutterBottom>
+                                        Social Links
+                                    </Typography>
+                                    <Box sx={{ display: 'flex', gap: 2 }}>
+                                        <Link href="https://www.facebook.com/uhurutek" target="_blank" rel="noopener noreferrer">
+                                            <FacebookIcon />
+                                        </Link>
+                                        <Link href="https://www.instagram.com/uhurutek" target="_blank" rel="noopener noreferrer">
+                                            <InstagramIcon />
+                                        </Link>
+                                        <Link href="https://www.linkedin.com/company/uhurutek" target="_blank" rel="noopener noreferrer">
+                                            <LinkedInIcon />
+                                        </Link>
+                                        <Link href="whatsapp://send?phone=8801846412513" target="_blank" rel="noopener noreferrer">
+                                            <WhatsAppIcon />
+                                        </Link>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} sm={3} md={2}>
-                        <Typography variant="subtitle1" color='white' gutterBottom>
-                            PRODUCT
-                        </Typography>
-                        <Link href="#" color="inherit" display="block">Features</Link>
-                        <Link href="#" color="inherit" display="block">Integrations</Link>
-                        <Link href="#" color="inherit" display="block">Pricing</Link>
-                        <Link href="#" color="inherit" display="block">FAQ</Link>
-                    </Grid>
-                    <Grid item xs={6} sm={3} md={2}>
-                        <Typography variant="subtitle1" color='white' gutterBottom>
-                            COMPANY
-                        </Typography>
-                        <Link href="#" color="inherit" display="block">About Us</Link>
-                        <Link href="#" color="inherit" display="block">Careers</Link>
-                        <Link href="#" color="inherit" display="block">Privacy Policy</Link>
-                        <Link href="#" color="inherit" display="block">Terms of Service</Link>
-                    </Grid>
-                    <Grid item xs={6} sm={3} md={2}>
-                        <Typography variant="subtitle1" color='white' gutterBottom>
-                            DEVELOPERS
-                        </Typography>
-                        <Link href="#" color="inherit" display="block">Public API</Link>
-                        <Link href="#" color="inherit" display="block">Documentation</Link>
-                        <Link href="#" color="inherit" display="block">Guides</Link>
-                    </Grid>
-                    <Grid item xs={6} sm={3} md={2}>
-                        <Typography variant="subtitle1" color='white' gutterBottom>
-                            SOCIAL MEDIA
-                        </Typography>
-                        <IconButton aria-label="Facebook" color="inherit" component="a" href={socialMediaLinks.facebook}>
-                            <FacebookIcon />
-                        </IconButton>
-                        <IconButton aria-label="Twitter" color="inherit" component="a" href={socialMediaLinks.twitter}>
-                            <TwitterIcon />
-                        </IconButton>
-                        <IconButton aria-label="Instagram" color="inherit" component="a" href={socialMediaLinks.instagram}>
-                            <InstagramIcon />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-                <Typography variant="body2" color="white" align="center" sx={{ pt: 4 }}>
-                    © 2024 Company Co. All rights reserved.
-                </Typography>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+
+            <Box sx={{ bgcolor: 'background.paper' }}>
+                <Container maxWidth="lg">
+                    <Typography variant="body2" align="center" color="text.secondary" sx={{ py: 2 }}>
+                        &copy; <span id="currentYear">{currentYear}</span>{' '}
+                        <strong>
+                            <span>UhuruTek</span>
+                        </strong>{' '}
+                        . All Rights Reserved | Developed by <strong> UhuruTek </strong>
+                    </Typography>
+                </Container>
+            </Box>
+        </footer>
     );
 };
 
--- src/Components/common/NavBar.js
+++ src/Components/common/NavBar.js
@@ -1,4 +1,4 @@
-import AdbIcon from '@mui/icons-material/Adb';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
 import { SwipeableDrawer } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
@@ -10,18 +10,15 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Link from 'next/link';
 import { useState } from 'react';
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = [{ 'name': 'Home', 'url': '/' }, { 'name': 'About', 'url': '/about' }, { 'name': 'Contact', 'url': '/contact' }];
 
 function NavBar() {
     const anchor = 'left';
-
     const [state, setState] = useState({
         left: false,
     });
-
     const toggleDrawer = (anchor, open) => (event) => {
         console.log("object");
         if (
@@ -35,132 +32,183 @@ function NavBar() {
         setState({ ...state, [anchor]: open });
     };
     return (
-        <AppBar sx={{ backgroundColor: "#9f4923" }} position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={toggleDrawer(anchor, true)}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            // anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            // open={Boolean(anchorElNav)}
-                            onClose={toggleDrawer}
+        <AppBar sx={{ backgroundColor: "#fff" }} position="sticky">
+            <Container>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
                             sx={{
-                                display: { xs: 'block', md: 'none' },
+                                mr: 2,
+                                display: { xs: 'flex', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+
+                                textDecoration: 'none',
+                                color: '#d76227',
+                                '&:hover': {
+
+                                    color: '#9f4923',
+                                },
                             }}
                         >
-                            <SwipeableDrawer
-                                anchor={anchor}
-                                open={state[anchor]}
-                                onClose={toggleDrawer(anchor, false)}
-                                onOpen={toggleDrawer(anchor, true)}
+                            LOGO
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+
+                            <Menu
+                                id="menu-appbar"
+                                // anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                // open={Boolean(anchorElNav)}
+                                onClose={toggleDrawer}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
                             >
-                                <Box
-                                    sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 200 }}
-                                    role="presentation"
-                                    onClick={toggleDrawer(anchor, false)}
-                                    onKeyDown={toggleDrawer(anchor, false)}
+                                <SwipeableDrawer
+                                    anchor={anchor}
+                                    open={state[anchor]}
+                                    onClose={toggleDrawer(anchor, false)}
+                                    onOpen={toggleDrawer(anchor, true)}
                                 >
-                                    <div style={{ textAlign: 'right', backgroundColor: '#9f4923', }}>
+                                    <Box
+                                        sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 200 }}
+                                        role="presentation"
+                                        onClick={toggleDrawer(anchor, false)}
+                                        onKeyDown={toggleDrawer(anchor, false)}
+                                    >
+                                        <div style={{ textAlign: 'right', backgroundColor: '#9f4923', }}>
+                                            <Button
+                                                sx={{
+                                                    display: 'inline-block',
+                                                    color: 'white',
+                                                    padding: '8px 16px',
+                                                    borderRadius: '4px',
+                                                }}
+                                            >
+                                                X
+                                            </Button>
+                                        </div>
+
+
+                                        {pages.map((page) => (
+                                            <Link key={page.name} href={page.url} style={{ textDecoration: 'none' }}>
+                                                <MenuItem onClick={toggleDrawer}>
+                                                    <Typography textAlign="center">{page.name}</Typography>
+                                                </MenuItem>
+                                            </Link>
+                                        ))}
+                                    </Box>
+                                </SwipeableDrawer>
+                            </Menu>
+                        </Box>
+                        {/* mobile */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+
+
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                {pages.map((page) => (
+                                    <Link key={page.name} href={page.url} style={{ textDecoration: 'none', display: { xs: 'none', md: 'flex' } }}>
                                         <Button
+                                            onClick={toggleDrawer}
                                             sx={{
-                                                display: 'inline-block',
-                                                color: 'white',
-                                                padding: '8px 16px',
-                                                borderRadius: '4px',
+                                                my: 2, display: { xs: 'none', md: 'block' }, color: '#d76227',
+                                                '&:hover': {
+
+                                                    color: '#9f4923',
+                                                },
                                             }}
                                         >
-                                            X
+                                            {page.name}
                                         </Button>
-                                    </div>
+                                    </Link>
+                                ))}
+                                <Link href="/registration">
+                                    <FacebookOutlinedIcon sx={{
+                                        display: {
+                                            xs: 'none', md: 'block', color: '#d76227',
+                                            '&:hover': {
+
+                                                color: '#9f4923',
+                                            },
+                                        }
+                                    }} />
+                                </Link>
+                                <Link href="/registration">
+                                    <FacebookOutlinedIcon sx={{
+                                        display: {
+                                            xs: 'none', md: 'block', color: '#d76227',
+                                            '&:hover': {
+
+                                                color: '#9f4923',
+                                            },
+                                        }
+                                    }} />
+                                </Link>
+                                <Link href="/registration">
+                                    <FacebookOutlinedIcon sx={{
+                                        display: {
+                                            xs: 'none', md: 'block', color: '#d76227',
+                                            '&:hover': {
+
+                                                color: '#9f4923',
+                                            },
+                                        }
+                                    }} />
+                                </Link>
+                                <Link href="/registration">
+                                    <FacebookOutlinedIcon sx={{
+                                        display: {
+                                            xs: 'none', md: 'block', color: '#d76227',
+                                            '&:hover': {
+
+                                                color: '#9f4923',
+                                            },
+                                        }
+                                    }} />
+                                </Link>
 
 
-                                    {pages.map((page) => (
-                                        <MenuItem key={page} onClick={toggleDrawer}>
-                                            <Typography textAlign="center">{page}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </Box>
-                            </SwipeableDrawer>
-                        </Menu>
-                    </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={toggleDrawer(anchor, true)}
+                                    sx={{
+                                        display: { xs: 'block', md: 'none' },
+                                        color: '#d76227',
+                                        '&:hover': {
 
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={toggleDrawer}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
-                    </Box>
+                                            color: '#9f4923',
+                                        },
+                                    }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                            <Button sx={{ my: 2, color: 'white' }}>Start Free Trial</Button>
-                            <Button sx={{ my: 2, color: 'white', display: { xs: 'none', md: 'block' } }}>Registration</Button>
-                        </div>
-                    </Box>
-                </Toolbar>
-            </Container>
+                            </div>
+                        </Box>
+                    </Toolbar>
+                </Container></Container>
         </AppBar >
     );
 }
