e6206eab2fa4594336f0ba21db5cfb3cb03895df
shant00 <sshanto.cse@gmail.com>
2023-12-27 21:57:22 +0600

    update footer

--- src/Components/home/PdfUpload.js
+++ src/Components/home/PdfUpload.js
@@ -1,25 +1,17 @@
 import { Grid, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles((theme) => ({
-    image: {
-        maxWidth: '100%',
-        height: 'auto',
-        // borderRadius: theme.spacing(1),
-    },
-}));
+
 
 const PdfUpload = ({ title, text }) => {
-    const classes = useStyles();
 
     return (
         <Grid container spacing={3} mb={8}>
             <Grid item xs={12} md={6}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                    src="https://via.placeholder.com/600x400" // Replace with your image URL
+                <img className='img-fluid'
+                    src="https://via.placeholder.com/600x400"
                     alt="Sample"
-                    className={classes.image}
+                    style={{ maxWidth: '100%', height: 'auto' }}
                 />
             </Grid>
             <Grid item xs={12} md={6}>
--- src/Components/home/SetUPTime.js
+++ src/Components/home/SetUPTime.js
@@ -1,4 +1,5 @@
 import { Button, Container, Typography } from '@mui/material';
+import Link from 'next/link';
 
 const SetUPTime = () => {
     return (
@@ -16,16 +17,23 @@ const SetUPTime = () => {
                 <Typography variant="body1" sx={{ marginBottom: 2, }}>
                     Spend 5 minutes to get a ChatGPT powered chatbot that works 24/7 to help you engage and retain more users and boost conversions.
                 </Typography>
-                <Button variant="contained" sx={{
-                    marginTop: 1,
-                    padding: '8px 32px',
-                    borderRadius: 24,
-                    fontSize: '1rem',
-                    fontWeight: 'bold',
-                    backgroundColor: '#9f4923',
-                }}>
-                    Get Started
-                </Button>
+                <Link href="/registration" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" sx={{
+                        marginTop: 1,
+                        padding: '8px 32px',
+                        borderRadius: 24,
+                        fontSize: '1rem',
+                        fontWeight: 'bold',
+                        backgroundColor: '#d76227',
+                        '&:hover': {
+
+                            backgroundColor: '#9f4923',
+                        },
+
+                    }}>
+                        Get Started
+                    </Button>
+                </Link>
             </Container>
 
         </div>
