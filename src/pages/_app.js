0684b1a5faf945378cde53435b374ff1930e26d0
shant00 <sshanto.cse@gmail.com>
2024-01-04 18:26:11 +0600

    font family change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,15 @@
-import '../styles/custom.style.css'
+import '../styles/custom.style.css';
+
+import { Inter } from 'next/font/google';
+
+// Define Inter font with subsets
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.className} font-open-sans font-raleway`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
+
